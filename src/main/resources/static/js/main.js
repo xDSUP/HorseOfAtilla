9186f767d8309cab7b5e17b88ae6db51c76ca8aa
Dgcim1 <gindra2001@gmail.com>
2022-02-28 16:23:42 +0300

    ui convert ns to ms

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -174,7 +174,7 @@ $(document).ready(function () {
       data: JSON.stringify({fire: json.fire, king: json.king, horse: json.horseStartPostiton, findType: json.findType}),
       success: function(data){
         json.path = data.path;
-        $(".state").html(`Success! Time: ${data.nanoTime} nanosec.`);
+        $(".state").html(`Success! Time: ${data.nanoTime / 1000000} ms.`);
         currentPos = 0;
         refresh();
       },
